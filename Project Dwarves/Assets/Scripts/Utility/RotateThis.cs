2593089c92ea7e9bef4deab23c0443204b866072
Stamperton <35846593+Stamperton@users.noreply.github.com>
2019-11-25 18:21:03 +0000

    Initial Commit
    Initial Commit. Includes Rewired and Substance Plugin.
    
    Substance Plugin failing to load. Could be due to Substance not existing on my home PC.

---
+++ Project Dwarves/Assets/Scripts/Utility/RotateThis.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateThis : MonoBehaviour
+{
+    public float rotateSpeed;
+    public Vector3 rotationAxis;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(rotationAxis.x, rotationAxis.y, rotationAxis.z) * Time.deltaTime * rotateSpeed);
+    }
+}
